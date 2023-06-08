51adbffdd30cc3a327b6d9314ce2dfae8326ca22
Alyson Hurt <alykat@gmail.com>
2023-06-08 17:46:08 -0400

    script updates, add readme

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # aqi-process
  
+This is a rudimentary script that pulls the latest AQI data from AirNow.gov.
+
+This script expects that you're in a Python environment ([we use Anaconda](https://blog.apps.npr.org/2013/06/06/how-to-setup-a-developers-environment.html)) and have `csvkit` installed.
+
+```
+git clone git@github.com:nprapps/aqi-process.git
+cd aqi-process
+conda create --name aqi-process python=3.9
+conda install -c conda-forge csvkit
+```
+
+To run it:
+
+```
+conda activate aqi-process
+bash aqi.sh
+```
+
+## Data notes
+
+The AQI datafile is a pipe-delimited `.dat` textfile, updated hourly. [Documentation lives here.](https://docs.airnowapi.org/docs/ReportingAreaFactSheet.pdf)
+
+Note: Forecast data beyond the current day does not exist for all reporting areas.
+
+[Additional data](http://files.airnowtech.org/?prefix=airnow/today/) and [documentation](https://docs.airnowapi.org/files) (API account may be required to access)
+
+## What the script does
+
+1. Download the datafile from the AQI site
+2. Add column headers
+3. Convert to a CSV and trim out extra columns / rows
+4. Query results just for a subset of cities
+5. Export the filtered set to JSON and copy it over to a dgnext graphic `aqi-cities-20230608` in the separate `graphics-js` repo
+    * _(Assumption: you work at NPR and already have this repo on your machine.)_
+
+## Further development
+
+TODO: Perhaps borrowing from the [covid data pipeline](https://github.com/nprapps/jhu-interceptor/tree/master), upload this to a server and set it on a cron, uploading the result to a folder on apps.npr.org.
--- aqi.sh
+++ aqi.sh
@@ -1,10 +1,33 @@
 timestamp=$(date +%s)
+
+# pull the data from EPA
 curl https://s3-us-west-1.amazonaws.com//files.airnowtech.org/airnow/today/reportingarea.dat > reportingarea.dat
-cp reportingarea.dat backups/reportingarea-${timestamp}.dat
+
+# add header rows
 echo "Issue date|Valid date|Valid time|Time zone|Record sequence|Data type|Primary|Reporting area|State code|Latitude|Longitude|Parameter name|AQI value|AQI category|Action day|Discussion|Forecast|Source" | cat - reportingarea.dat > working.dat
-in2csv -d "|" -f csv working.dat > aqi.csv
-csvcut -C "Valid date","Valid time","Latitude","Longitude","Action day","Discussion" aqi.csv | csvgrep -c "Primary" -m "True" > temp.csv
+
+# archive the datafile
+cp reportingarea.dat backups/reportingarea-${timestamp}.dat
+
+# clear out weird lone quote mark that breaks things and makes NYC disappear
+find='|"|'
+replace='||'
+sed "s/${find}/${replace}/g" working.dat > temp.dat
+rm working.dat
+mv temp.dat working.dat
+
+# turn it into a CSV
+in2csv -I -d "|" -f csv working.dat > aqi.csv
+
+# trim out extra columns and filter to only primary data rows
+csvcut -C "Issue date","Latitude","Longitude","Action day","Discussion" aqi.csv | csvgrep -c "Primary" -m "Y" > temp.csv
+rm aqi.csv
 mv temp.csv aqi.csv
+
+# dump it into a squlite database and filter only the cities we're interested in
 csvsql --db sqlite:///aqi.db --insert --overwrite aqi.csv
-csvsql --query "select * from aqi where (\"Reporting area\" = \"Metropolitan Washington\") OR (\"Reporting area\" = \"New York City Region\") OR (\"Reporting area\" = \"Philadelphia\") OR (\"Reporting area\" = \"Boston\") OR (\"Reporting area\" = \"Chicago\") OR (\"Reporting area\" = \"Pittsburgh\") OR (\"Reporting area\" = \"Metro Baltimore\");" aqi.csv > filtered.csv
+csvsql --query "select * from aqi where (\"Reporting area\" = \"Metropolitan Washington\") OR (\"Reporting area\" = \"New York City Region\") OR (\"Reporting area\" = \"Philadelphia\") OR (\"Reporting area\" = \"Boston\") OR (\"Reporting area\" = \"Chicago\") OR (\"Reporting area\" = \"Pittsburgh\") OR (\"Reporting area\" = \"Metro Baltimore\") OR (\"Reporting area\" = \"Detroit\");" aqi.csv > filtered.csv
 csvjson filtered.csv > filtered.json
+
+# copy the data over to a dngext graphic
+cp filtered.json ../graphics-js/aqi-cities-20230608/
